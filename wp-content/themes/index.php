785513b2d9c58630f9740c3fdde419d939421c44
admin <zacharyjsanders@gmail.com>
2019-03-26 20:45:17 -0600

    Adding Winifred theme

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,2 +1,12 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+   <?php wp_head(); ?>
+</head>
+
+<body>
+    <div id="app">
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
