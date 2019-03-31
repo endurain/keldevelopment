a1da70a8080f4465b93b7195ac36e689ddb7f511
admin <zacharyjsanders@gmail.com>
2019-03-31 13:39:47 -0600

    Adding ACF & wp rest api

--- wp-content/themes/Winifred/functions.php
+++ wp-content/themes/Winifred/functions.php
@@ -1,6 +1,30 @@
-function enqueue_scripts(){
-   wp_enqueue_script('vue', 'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js', [], '2.5.17');
+<?php
+/**
+ * Winifred functions and definitions
+ *
+ *
+ *
+ * @package WordPress
+ * @subpackage Winifred
+ * @since 1.0.0
+ */
 
+
+function sections_endpoint ( $request_data ) {
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' =>-1,
+    'numberposts' =>-1
+  );
+  $posts = get_posts($args);
+  foreach ($posts as $key => $post) {
+    $posts [$key]->acf = get_fields($posts->ID);
+  }
+  return $posts;
 }
-
-<!-- some test content -->
+add_action('rest_api_init', function () {
+  register_rest_route( 'sections/v1', '/post/', array(
+    'methods' => 'GET',
+    'callback' => 'sections_endpoint'
+  ));
+});
